f19ec0899b11dece4f00ab7f20a2aabbcca5f12b
Pavel Iliev <pavel.r.iliev@gmail.com>
2022-10-15 10:03:12 +0300

    Project Initialization

---
+++ YouTube_Project/src/main/java/com/youtube_project/YouTubeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.youtube_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouTubeProjectApplication.class, args);
+    }
+
+}
