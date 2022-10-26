cf1345a53169c23d6b44ff8f0c9ca374d476d1c6
Pavel Iliev <pavel.r.iliev@gmail.com>
2022-10-26 18:01:46 +0300

    Feature added - Uploading a profile image and background image to Amazon S3

--- YouTube_Project/src/main/java/com/youtube_project/services/UserService.java
+++ YouTube_Project/src/main/java/com/youtube_project/services/UserService.java
@@ -1,8 +1,9 @@
 package com.youtube_project.services;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.rest.lookups.v1.PhoneNumber;
 import com.youtube_project.model.dtos.user.*;
 import com.youtube_project.model.entities.User;
 import com.youtube_project.model.exceptions.BadRequestException;
@@ -10,6 +11,7 @@ import com.youtube_project.model.exceptions.NotFoundException;
 import com.youtube_project.model.exceptions.UnauthorizedException;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -20,18 +22,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-
 
 
 @Service
@@ -41,7 +43,7 @@ public class UserService extends AbstractService {
     public static final String VALID_PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=.!])(?=\\S+$).{8,90}$";
     public static final String VALID_EMAIL_ADDRESS_REGEX = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"; // regex RFC 5322 for Email Validation
     public static final int USER_EMAIL_MAX_LENGTH = 90;
-    public static final String USER_VALID_PHONE_NUMBER =  "\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}";
+    public static final String USER_VALID_PHONE_NUMBER = "\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}";
     public static final int USER_ADDITIONAL_INFO_MAX_LENGTH = 2000;
     public static final int TOTAL_VALID_NUMBER_OF_ROLES = 2; // number of existing roles in DB (consecutive numbers)
     public static final int TOTAL_VALID_NUMBER_OF_GENDERS = 3; // number of existing genders in DB (consecutive numbers)
@@ -52,10 +54,16 @@ public class UserService extends AbstractService {
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private AmazonS3 s3Client;
+    @Value("${application.bucket.name}" + "/profile_pictures")
+    private String bucketNameForProfilePictures;
+    @Value("${application.bucket.name}" + "/background_picture_user")
+    private String bucketNameForProfileBackGroundImage;
 
     public UserResponseDTO register(UserRegistrationDTO userDTO) {
         // validate user information from json (editable)
-        UserEditProfileDTO userEdit = modelMapper.map(userDTO,UserEditProfileDTO.class);
+        UserEditProfileDTO userEdit = modelMapper.map(userDTO, UserEditProfileDTO.class);
         validateEditableInfo(userEdit);
         // validate additional user info (not editable) (password/confirm_password and e-mail)
         validateRegistrationInfo(userDTO);
@@ -89,6 +97,7 @@ public class UserService extends AbstractService {
 
         return modelMapper.map(u, UserResponseDTO.class);
     }
+
     public UserResponseDTO login(UserLoginWithEmailDTO userLoginWithEmailDTO) {
         String email = userLoginWithEmailDTO.getEmail().trim();
         String loginPassword = userLoginWithEmailDTO.getPassword().trim();
@@ -101,7 +110,7 @@ public class UserService extends AbstractService {
         }
         User u = user.get(0);
 
-        if(!passwordEncoder.matches(loginPassword,u.getPassword())){
+        if (!passwordEncoder.matches(loginPassword, u.getPassword())) {
             throw new UnauthorizedException("Wrong credentials");
         }
 
@@ -120,7 +129,7 @@ public class UserService extends AbstractService {
         }
         User u = user.get(0);
 
-        if(!passwordEncoder.matches(userLoginWithPhoneNumDTO.getPassword(),u.getPassword())){
+        if (!passwordEncoder.matches(userLoginWithPhoneNumDTO.getPassword(), u.getPassword())) {
             throw new UnauthorizedException("Wrong credentials");
         }
 
@@ -136,8 +145,8 @@ public class UserService extends AbstractService {
     }
 
     public List<UserResponseDTO> getAllUsersByName(String firstName, String lastName, int pageNumber, int rowNumbers) {
-        Pageable page = PageRequest.of(pageNumber,rowNumbers);
-        return userRepository.findAllByFirstNameAndLastName(firstName, lastName,page).stream().map(u -> modelMapper.map(u, UserResponseDTO.class)).collect(Collectors.toList());
+        Pageable page = PageRequest.of(pageNumber, rowNumbers);
+        return userRepository.findAllByFirstNameAndLastName(firstName, lastName, page).stream().map(u -> modelMapper.map(u, UserResponseDTO.class)).collect(Collectors.toList());
     }
 
     public boolean deleteUserAccount(long id) {
@@ -151,7 +160,7 @@ public class UserService extends AbstractService {
         return true;
     }
 
-    private boolean validateEditableInfo(UserEditProfileDTO user){
+    private boolean validateEditableInfo(UserEditProfileDTO user) {
 
         String firstName = user.getFirstName();
         String lastName = user.getLastName();
@@ -177,13 +186,12 @@ public class UserService extends AbstractService {
             if (dateOfBirthFormatted.isAfter(LocalDate.now().minusYears(13))) {
                 throw new UnauthorizedException("You should be at least 13 years old");
             }
-        }
-        catch (DateTimeParseException ex){
+        } catch (DateTimeParseException ex) {
             throw new BadRequestException("Invalid date format!");
         }
 
         //validate phoneNumber
-        if(!phoneNumber.matches(USER_VALID_PHONE_NUMBER)){
+        if (!phoneNumber.matches(USER_VALID_PHONE_NUMBER)) {
             throw new BadRequestException("Phone number not valid!");
         }
         // check if phone number is registered
@@ -193,21 +201,21 @@ public class UserService extends AbstractService {
         // check if roleId is valid
         boolean validRoleId = false;
         for (int i = 1; i <= TOTAL_VALID_NUMBER_OF_ROLES; i++) {
-            if(roleId == i){
+            if (roleId == i) {
                 validRoleId = true;
             }
         }
-        if(!validRoleId){
+        if (!validRoleId) {
             throw new BadRequestException("Invalid role!");
         }
         //Validate genderId
         boolean validGenderId = false;
         for (int i = 1; i <= TOTAL_VALID_NUMBER_OF_GENDERS; i++) {
-            if(genderId == i){
+            if (genderId == i) {
                 validGenderId = true;
             }
         }
-        if(!validGenderId){
+        if (!validGenderId) {
             throw new BadRequestException("Invalid gender!");
         }
         //Additional info validation
@@ -217,7 +225,7 @@ public class UserService extends AbstractService {
         return true;
     }
 
-    private boolean validateRegistrationInfo(UserRegistrationDTO user){
+    private boolean validateRegistrationInfo(UserRegistrationDTO user) {
 
         String password = user.getPassword();
         String confirmPassword = user.getConfirmPassword();
@@ -228,18 +236,18 @@ public class UserService extends AbstractService {
             throw new BadRequestException("Invalid password!");
         }
         // check if password match confirm password
-        if(!password.equals(confirmPassword)){
+        if (!password.equals(confirmPassword)) {
             throw new BadRequestException("Password mismatch!");
         }
         //check if e-mail is valid
-        if(email.trim().isEmpty() || email.length() > USER_EMAIL_MAX_LENGTH){
+        if (email.trim().isEmpty() || email.length() > USER_EMAIL_MAX_LENGTH) {
             throw new BadRequestException("Invalid e-mail!");
         }
-        if(!email.matches(VALID_EMAIL_ADDRESS_REGEX)){
+        if (!email.matches(VALID_EMAIL_ADDRESS_REGEX)) {
             throw new BadRequestException("Invalid e-mail!");
         }
         //check if email is already registered
-        if(userRepository.findAllByEmail(user.getEmail()).size() > 0){
+        if (userRepository.findAllByEmail(user.getEmail()).size() > 0) {
             throw new BadRequestException("Email already registered!");
         }
         return true;
@@ -258,25 +266,29 @@ public class UserService extends AbstractService {
         user.setRoleId(dto.getRoleId());
         user.setAdditionalInfo(dto.getAdditionalInfo());
         userRepository.save(user);
-        return modelMapper.map(user,UserEditProfileDTO.class);
+        return modelMapper.map(user, UserEditProfileDTO.class);
     }
 
-    public Set<UserResponseDTO> getSubscriptions(long userId){
+    public Set<UserResponseDTO> getSubscriptions(long userId) {
         User u = getUserById(userId);
         Set<User> followers = u.getSubscriptions();
-        return followers.stream().map(user -> modelMapper.map(user,UserResponseDTO.class)).collect(Collectors.toSet());
+        return followers.stream().map(user -> modelMapper.map(user, UserResponseDTO.class)).collect(Collectors.toSet());
     }
-    public List<UserResponseDTO> getFollowers(long userId){
+
+    public List<UserResponseDTO> getFollowers(long userId) {
         User u = getUserById(userId);
         List<User> subscriptions = u.getFollowers();
-        return subscriptions.stream().map(user -> modelMapper.map(user,UserResponseDTO.class)).collect(Collectors.toList());
+        return subscriptions.stream().map(user -> modelMapper.map(user, UserResponseDTO.class)).collect(Collectors.toList());
     }
 
 
-    public String subscribe(long subscribeToId,long uid) {
+    public String subscribe(long subscribeToId, long uid) {
         User u = getUserById(uid);
         User userToSubscribeTo = getUserById(subscribeToId);
-        if (u.getSubscriptions().contains(userToSubscribeTo)){
+        if (!u.isVerified()){
+            throw new UnauthorizedException("Please verify your account first!");
+        }
+        if (u.getSubscriptions().contains(userToSubscribeTo)) {
             throw new BadRequestException("You are already subscribed to this user");
         }
         u.getSubscriptions().add(userToSubscribeTo);
@@ -287,7 +299,10 @@ public class UserService extends AbstractService {
     public String unsubscribe(long subscribeToId, long uid) {
         User u = getUserById(uid);
         User userToSubscribeTo = getUserById(subscribeToId);
-        if (!u.getSubscriptions().contains(userToSubscribeTo)){
+        if (!u.isVerified()){
+            throw new UnauthorizedException("Please verify your account first!");
+        }
+        if (!u.getSubscriptions().contains(userToSubscribeTo)) {
             throw new BadRequestException("You are already unsubscribed from this user");
         }
         u.getSubscriptions().remove(userToSubscribeTo);
@@ -296,49 +311,67 @@ public class UserService extends AbstractService {
     }
 
     public String uploadProfilePhoto(MultipartFile photo, long uid) {
-            User user = getUserById(uid);
-            String extension = FilenameUtils.getExtension(photo.getOriginalFilename());
-            String fileURL = "uploads" + File.separator + "profile_pictures" +File.separator+ "profile_photo_user" + "_" + uid + "." + extension;
+        User user = getUserById(uid);
+        String extension = FilenameUtils.getExtension(photo.getOriginalFilename());
+        String photoURL = "uploads" + File.separator + "profile_pictures" + File.separator + "profile_photo_user" + "_" + uid + "." + extension;
+        saveAndReplacePhoto(photo, user, photoURL, bucketNameForProfilePictures);
+        user.setProfilePhoto(photoURL);
+        userRepository.save(user);
 
-            saveAndReplacePhotoLocally(photo,fileURL,user);
+        return "Successfully uploaded/uploaded your profile photo. Image name: " + photo.getName();
+    }
 
-            user.setProfilePhoto(fileURL);
-            userRepository.save(user);
+    private File convertMultipartFileToFIle(MultipartFile file) {
+        File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            System.out.println("Problem occurred while converting file." + e.getMessage());
+        }
 
-            return "Successfully uploaded " + photo.getName();
+        return convertedFile;
     }
 
     public String uploadBackgroundPhoto(MultipartFile photo, long uid) {
         User user = getUserById(uid);
         String extension = FilenameUtils.getExtension(photo.getOriginalFilename());
-        String fileURL = "uploads" + File.separator + "background_pictures" +File.separator+ "background_picture_user" + "_" + uid + "." + extension;
+        String fileURL = "uploads" + File.separator + "background_pictures" + File.separator + "background_picture_user" + "_" + uid + "." + extension;
 
-        saveAndReplacePhotoLocally(photo,fileURL,user);
+        saveAndReplacePhoto(photo, user, fileURL, bucketNameForProfileBackGroundImage);
 
         user.setBackgroundImage(fileURL);
         userRepository.save(user);
 
-        return "Successfully uploaded " + photo.getName();
+        return "Successfully uploaded/updated your background image. Image name: " + photo.getName();
     }
 
-    private void saveAndReplacePhotoLocally(MultipartFile photo,String fileURL,User user) {
-        try {
-            File f = new File(fileURL);
-            if(!f.exists()){
-                    Files.copy(photo.getInputStream(),f.toPath());
-            }
-            else {
-                f.delete();
-                Files.copy(photo.getInputStream(),f.toPath());
-            }
-        } catch (IOException e) {
-            throw new BadRequestException(e.getMessage(),e);
-        }
-        if(user.getProfilePhoto() != null){
-            File old = new File(user.getProfilePhoto());
-            old.delete();
-        }
+    private void saveAndReplacePhoto(MultipartFile photo, User user, String photoURL, String bucketName) {
+        delete(photoURL, user.getId(), bucketName);
+        File file1 = convertMultipartFileToFIle(photo);
+        s3Client.putObject(new PutObjectRequest(bucketName, photoURL, file1));
+        file1.delete();
     }
+
+    public void delete(String photoURL, long loggedUserId, String bucketName) {
+        User u = getUserById(loggedUserId);
+        String photo = null;
+        if (bucketName.equals(bucketNameForProfilePictures)) {
+            photo = u.getProfilePhoto();
+        } else {
+            photo = u.getBackgroundImage();
+        }
+        if (photo != null) {
+
+            if (!photo.equals(photoURL)) {
+                throw new UnauthorizedException("Wrong photo URL");
+            }
+            if (s3Client.doesObjectExist(bucketName, photoURL)) {
+                s3Client.deleteObject(bucketName, photoURL);
+            }
+        }
+
+    }
+
 
     public UserResponseDTO verifyRegistration(String encryptedId) {
         int id = 0;
@@ -357,8 +390,9 @@ public class UserService extends AbstractService {
         javaMailSender.send(msg);
         userRepository.save(user);
 
-        return modelMapper.map(user,UserResponseDTO.class);
+        return modelMapper.map(user, UserResponseDTO.class);
     }
+
     public ResponseEntity<String> forgottenPassword(String email) {
         User user = getUserByEmail(email);
         String newPassword = generateNewPassword(LENGTH);
@@ -386,10 +420,10 @@ public class UserService extends AbstractService {
 
     public String changePassword(UserChangePasswordDTO dto, long loggedUserId) {
         User user = getUserById(loggedUserId);
-        if(!passwordEncoder.matches(dto.getCurrentPassword(),user.getPassword())){
+        if (!passwordEncoder.matches(dto.getCurrentPassword(), user.getPassword())) {
             throw new BadRequestException("Password mismatch!");
         }
-        if(!dto.getPasswordNew().equals(dto.getPasswordNewConfirm())){
+        if (!dto.getPasswordNew().equals(dto.getPasswordNewConfirm())) {
             throw new BadRequestException("Password mismatch!");
         }
         if (!dto.getPasswordNew().matches(VALID_PASSWORD_REGEX)) {
