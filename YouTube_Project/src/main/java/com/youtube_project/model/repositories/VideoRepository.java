b982b4bced118c1cb88778c87f6ce41d561ba299
Pavel Iliev <pavel.r.iliev@gmail.com>
2022-10-26 18:00:40 +0300

    Added pagination

--- YouTube_Project/src/main/java/com/youtube_project/model/repositories/VideoRepository.java
+++ YouTube_Project/src/main/java/com/youtube_project/model/repositories/VideoRepository.java
@@ -2,15 +2,17 @@ package com.youtube_project.model.repositories;
 
 import com.youtube_project.model.entities.User;
 import com.youtube_project.model.entities.Video;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video,Long> {
 
-    List<Video> findAllByTitle(String title);
+    @Query("select v from Video v where v.title LIKE %:title%")
+    List<Video> findAllByTitle(String title, Pageable page);
     List<Video> findAllByOwner(User user);
 }
