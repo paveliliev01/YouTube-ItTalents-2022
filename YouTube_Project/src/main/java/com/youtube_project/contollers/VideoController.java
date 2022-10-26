2210f29155b32b63739c8ad36c8ad1806a22ef4e
Pavel Iliev <pavel.r.iliev@gmail.com>
2022-10-26 17:59:00 +0300

    Feature added - Uploading a video to Amazon S3

--- YouTube_Project/src/main/java/com/youtube_project/contollers/VideoController.java
+++ YouTube_Project/src/main/java/com/youtube_project/contollers/VideoController.java
@@ -2,7 +2,6 @@ package com.youtube_project.contollers;
 
 import com.youtube_project.model.dtos.video.VideoResponseDTO;
 import com.youtube_project.model.dtos.video.VideoSimpleResponseDTO;
-import com.youtube_project.model.entities.Video;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/videos")
-public class VideoController extends MasterController{
+public class VideoController extends MasterController {
 
 
     @PostMapping("/upload")
@@ -21,36 +20,39 @@ public class VideoController extends MasterController{
                               @RequestParam(value = "title") String title,
                               @RequestParam(value = "description") String description,
                               @RequestParam(value = "Private") Boolean isPrivate,
-                              HttpServletRequest request){
+                              HttpServletRequest request) {
         sessionManager.validateLogin(request);
         long loggedUserId = sessionManager.getSessionUserId(request);
-        return videoService.upload(loggedUserId,video,title,description,isPrivate);
+        return videoService.upload(loggedUserId, video, title, description, isPrivate);
     }
 
     @GetMapping("/{vid}")
     @ResponseStatus(HttpStatus.FOUND)
-    public VideoResponseDTO getVideoById(@PathVariable long vid){
+    public VideoResponseDTO getVideoById(@PathVariable long vid) {
         return videoService.getById(vid);
     }
 
     @GetMapping("/search")
     @ResponseStatus(HttpStatus.FOUND)
-    public List<VideoResponseDTO> getByTitle(@RequestParam(value = "title") String title){
-        return videoService.getByTitle(title);
+    public List<VideoResponseDTO> getByTitle(@RequestParam(value = "title") String title,
+                                             @RequestParam(defaultValue = "0") int pageNumber,
+                                             @RequestParam(defaultValue = "1") int rowNumbers)
+    {
+        return videoService.getByTitle(title,pageNumber,rowNumbers);
     }
 
     @PostMapping("/{vid}/like")
-    public String likeVideo(@PathVariable long vid,HttpServletRequest request){
+    public String likeVideo(@PathVariable long vid, HttpServletRequest request) {
         sessionManager.validateLogin(request);
         long loggedUserId = sessionManager.getSessionUserId(request);
-        return videoService.reactToVideo(vid,loggedUserId, LIKE);
+        return videoService.reactToVideo(vid, loggedUserId, LIKE);
     }
 
     @PostMapping("/{vid}/dislike")
-    public String dislikeVideo(@PathVariable long vid,HttpServletRequest request){
+    public String dislikeVideo(@PathVariable long vid, HttpServletRequest request) {
         sessionManager.validateLogin(request);
         long loggedUserId = sessionManager.getSessionUserId(request);
-        return videoService.reactToVideo(vid,loggedUserId, DISLIKE);
+        return videoService.reactToVideo(vid, loggedUserId, DISLIKE);
     }
 
     @GetMapping("/likedVideos")
@@ -68,20 +70,21 @@ public class VideoController extends MasterController{
     }
 
     @PostMapping("/{vid}/watch")
-    public VideoResponseDTO watchVideo(@PathVariable long vid,HttpServletRequest request){
+    public VideoResponseDTO watchVideo(@PathVariable long vid, HttpServletRequest request) {
         sessionManager.validateLogin(request);
         long loggedUserId = sessionManager.getSessionUserId(request);
-        return videoService.watch(vid,loggedUserId);
+        return videoService.watch(vid, loggedUserId);
     }
 
-    @PutMapping("/{vid}/delete")
-    public String deleteVideo(@PathVariable long vid,HttpServletRequest request){
+    @DeleteMapping("/{vid}/delete")
+    public String deleteVideo(@PathVariable long vid, HttpServletRequest request) {
         sessionManager.validateLogin(request);
         long loggedUserId = sessionManager.getSessionUserId(request);
-        return videoService.delete(vid,loggedUserId);
+        return videoService.delete(vid, loggedUserId);
     }
+
     @GetMapping("/{uid}/uploads")
-    public List<VideoResponseDTO> getUserUploads(@PathVariable long uid, HttpServletRequest request){
+    public List<VideoResponseDTO> getUserUploads(@PathVariable long uid, HttpServletRequest request) {
         sessionManager.validateLogin(request);
         return videoService.getUploads(uid);
     }
--- YouTube_Project/src/main/java/com/youtube_project/services/VideoService.java
+++ YouTube_Project/src/main/java/com/youtube_project/services/VideoService.java
@@ -1,25 +1,32 @@
 package com.youtube_project.services;
 
-import com.youtube_project.model.dtos.user.UserResponseDTO;
-import com.youtube_project.model.dtos.video.*;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.youtube_project.model.dtos.video.VideoDAO;
+import com.youtube_project.model.dtos.video.VideoResponseDTO;
+import com.youtube_project.model.dtos.video.VideoSimpleResponseDTO;
+import com.youtube_project.model.entities.User;
 import com.youtube_project.model.entities.Video;
 import com.youtube_project.model.exceptions.BadRequestException;
 import com.youtube_project.model.exceptions.NotFoundException;
 import com.youtube_project.model.exceptions.UnauthorizedException;
 import com.youtube_project.model.relationships.videoreactions.VideoReaction;
 import com.youtube_project.model.relationships.videoreactions.VideoReactionKey;
-import com.youtube_project.model.entities.User;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,73 +36,90 @@ public class VideoService extends AbstractService {
     public static final int MAX_DESCRIPTION_LENGTH = 200;
 
     @Autowired
-    VideoDAO videoDAO;
+    private VideoDAO videoDAO;
+    @Autowired
+    private AmazonS3 s3Client;
+    @Value("${application.bucket.name}" + "/videos")
+    private String bucketName;
 
-    public VideoResponseDTO getById(long id){
+
+    public VideoResponseDTO getById(long id) {
         Video v = getVideoById(id);
         VideoResponseDTO dto = videoToResponseVideoDTO(v);
         return dto;
     }
 
 
-    private void validateVideoInfo(MultipartFile file,String title,String description){
-        if(file.isEmpty()){
+    private void validateVideoInfo(MultipartFile file, String title, String description) {
+        if (file.isEmpty()) {
             throw new BadRequestException("The file is not attached!");
         }
-        if(title.isEmpty() || title.length() > MAX_TITLE_LENGTH){
+        if (title.isEmpty() || title.length() > MAX_TITLE_LENGTH) {
             throw new BadRequestException("Incorrect video title");
         }
-        if (description.isEmpty() || description.length() > MAX_DESCRIPTION_LENGTH){
+        if (description.isEmpty() || description.length() > MAX_DESCRIPTION_LENGTH) {
             throw new BadRequestException("Incorrect description!");
         }
         if (!file.getContentType().equals("video/mp4")) {
             throw new BadRequestException("Invalid video format!");
         }
     }
-    public String upload(long loggedUserId,MultipartFile file,String title,String description,boolean isPrivate) {
-        try {
-            title = title.trim();
-            description = description.trim();
-            validateVideoInfo(file,title,description);
 
-            User user = getUserById(loggedUserId);
-            String extension = FilenameUtils.getExtension(file.getOriginalFilename());
-            String videoURL = "uploads" + File.separator + "videos" +File.separator+ System.nanoTime() + "_" + loggedUserId + "." + extension;
-            File f = new File(videoURL);
-            if(!f.exists()){
-                Files.copy(file.getInputStream(),f.toPath());
-            }else {
-                //this should never happen!
-                throw new BadRequestException("The file already exists!");
-            }
-
-            Video video = new Video();
-            video.setTitle(title);
-            video.setDescription(description);
-            video.setOwner(user);
-            video.setDateOfUpload(LocalDate.now());
-            video.setVideoURL(videoURL);
-            video.setPrivate(isPrivate);
-
-            videoRepository.save(video);
-
-            return "Successfully uploaded " + file.getName();
+    private File convertMultipartFileToFIle(MultipartFile file) {
+        File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
         } catch (IOException e) {
-            throw new BadRequestException(e.getMessage(),e);
+            System.out.println("CIRK");
         }
+
+        return convertedFile;
     }
 
-    public List<VideoResponseDTO> getByTitle(String title) {
-        List<Video> videos = videoRepository.findAllByTitle(title);
-        List<VideoResponseDTO> videoDTOS = new ArrayList<>();
-        for (Video v : videos){
-            videoDTOS.add(videoToResponseVideoDTO(v));
+    public String upload(long loggedUserId, MultipartFile file, String title, String description, boolean isPrivate) {
+
+        User user = getUserById(loggedUserId);
+        if (!user.isVerified()) {
+            throw new UnauthorizedException("Please verify your account first!");
         }
-        return videoDTOS;
+
+        title = title.trim();
+        description = description.trim();
+        validateVideoInfo(file, title, description);
+
+
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        String videoURL = "uploads" + File.separator + "videos" + File.separator + System.nanoTime() + "_" + loggedUserId + "." + extension;
+
+        File file1 = convertMultipartFileToFIle(file);
+        s3Client.putObject(new PutObjectRequest(bucketName, videoURL, file1));
+        file1.delete();
+
+        Video video = new Video();
+        video.setTitle(title);
+        video.setDescription(description);
+        video.setOwner(user);
+        video.setDateOfUpload(LocalDate.now());
+        video.setVideoURL(videoURL);
+        video.setPrivate(isPrivate);
+
+        videoRepository.save(video);
+
+        return "Successfully uploaded " + file.getName();
+    }
+
+    public List<VideoResponseDTO> getByTitle(String title, int pageNumber, int rowNumbers) {
+        Pageable page = PageRequest.of(pageNumber, rowNumbers);
+
+        return videoRepository.findAllByTitle(title,page).stream().map(this::videoToResponseVideoDTO).collect(Collectors.toList());
     }
 
     public String reactToVideo(long videoId, long userId, char reaction) {
         User u = getUserById(userId);
+        if (!u.isVerified()) {
+            throw new UnauthorizedException("Please verify your account first!");
+        }
+
         VideoReaction videoReaction = new VideoReaction();
         VideoReactionKey videoReactionKey = new VideoReactionKey();
         videoReactionKey.setVideoId(videoId);
@@ -107,7 +131,7 @@ public class VideoService extends AbstractService {
         videoReaction.setReaction(reaction);
 
         if (videoReactionRepository.findById(videoReactionKey).isPresent() &&
-                videoReactionRepository.findById(videoReactionKey).get().getReaction() == reaction){
+                videoReactionRepository.findById(videoReactionKey).get().getReaction() == reaction) {
             videoReactionRepository.delete(videoReaction);
             return "Successfully removed reaction";
         }
@@ -128,7 +152,7 @@ public class VideoService extends AbstractService {
     public VideoResponseDTO watch(long vid, long loggedUserId) {
         Video video = getVideoById(vid);
         User user = getUserById(loggedUserId);
-        if(!user.getWatchedVideos().contains(video)){
+        if (!user.getWatchedVideos().contains(video)) {
             user.getWatchedVideos().add(video);
             userRepository.save(user);
         }
@@ -138,13 +162,14 @@ public class VideoService extends AbstractService {
 
     public String delete(long vid, long loggedUserId) {
         Video video = getVideoById(vid);
-        if(video.getOwner().getId() != loggedUserId){
+        if (video.getOwner().getId() != loggedUserId) {
             throw new UnauthorizedException("You're not the owner of the video");
         }
-        File f = new File(video.getVideoURL());
-        f.delete();
-        videoRepository.delete(video);
-        return "Video has been deleted successfully";
+        if (s3Client.doesObjectExist(bucketName, video.getVideoURL())) {
+            s3Client.deleteObject(bucketName, video.getVideoURL());
+            return video.getVideoURL() + " removed";
+        }
+        return "File not found! Deletion unsuccessful!";
     }
 
     public List<VideoSimpleResponseDTO> getMostWatched(int rows, int pageNumber) {
@@ -173,8 +198,12 @@ public class VideoService extends AbstractService {
 
     public List<VideoResponseDTO> getUploads(long uid) {
         User user = getUserById(uid);
+        if (!user.isVerified()) {
+            throw new UnauthorizedException("Please verify your account first!You cannot upload any videos if your account is not verified!");
+        }
+
         List<Video> videos = videoRepository.findAllByOwner(user);
-        List<VideoResponseDTO> videosDto = videos.stream().map(v -> modelMapper.map(v,VideoResponseDTO.class)).collect(Collectors.toList());
+        List<VideoResponseDTO> videosDto = videos.stream().map(v -> modelMapper.map(v, VideoResponseDTO.class)).collect(Collectors.toList());
         return videosDto;
     }
 
