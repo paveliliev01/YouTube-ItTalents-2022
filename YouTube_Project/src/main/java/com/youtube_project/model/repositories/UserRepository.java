5cd58f2a7311d23436471a10f7de38fb3ad2d4da
Pavel Iliev <pavel.r.iliev@gmail.com>
2022-10-26 18:05:37 +0300

    Custom queries added in repository

--- YouTube_Project/src/main/java/com/youtube_project/model/repositories/UserRepository.java
+++ YouTube_Project/src/main/java/com/youtube_project/model/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.youtube_project.model.repositories;
 import com.youtube_project.model.entities.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     List<User> findAllByPhoneNumber(String phoneNumber);
 
+    @Query("select u from User u where u.firstName  LIKE %:firstName% and u.lastName LIKE %:lastName%")
     List<User> findAllByFirstNameAndLastName(String firstName, String lastName, Pageable pageable);
 
     Optional<User> findUserByEmail(String email);
